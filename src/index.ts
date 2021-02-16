55890a0a29a57475462eef9fbe31ab526244a13e
Andrew Andersen <andersac88@gmail.com>
2021-02-16 08:25:57 -0600

    end of train ride

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,8 @@
 import { CsvFileReader } from './CsvFileReader';
+import { MatchResult } from './MatchResult';
 
 const reader = new CsvFileReader('football.csv');
 reader.read();
-
-console.log(reader.data);
-
-enum MatchResult {
-	Homewin = 'H',
-	AwayWin = 'A',
-	Draw = 'D',
-}
 
 let manUnitedWins = 0;
 
