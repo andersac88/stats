5b73c9553ae3ad57e0d55dc6c5d8836db64ef6a2
Andrew Andersen <andersac88@gmail.com>
2021-02-16 21:53:16 -0600

    updates

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,17 @@
-import { CsvFileReader } from './CsvFileReader';
+import { MatchReader } from './MatchReader';
 import { MatchResult } from './MatchResult';
 
-const reader = new CsvFileReader('football.csv');
+const reader = new MatchReader('football.csv');
 reader.read();
+
+console.log(reader.data[0][5]);
 
 let manUnitedWins = 0;
 
 for (let match of reader.data) {
-	if (match[1] === 'Manchester United' && match[5] === MatchResult.Homewin) {
+	if (match[1] === 'Man United' && match[5] === MatchResult.Homewin) {
 		manUnitedWins++;
-	} else if (
-		match[2] === 'Manchester United' &&
-		match[5] === MatchResult.AwayWin
-	) {
+	} else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
 		manUnitedWins++;
 	}
 }
